c9b5fd8e6737d4c0cbf0bfd3bfd8c9eddbb589a7
Idan <idansano10@gmail.com>
2026-02-08 18:02:27 +0200

    almost finished

---
+++ frontend/script.js
@@ -0,0 +1,19 @@
+const APIKEY = "e14892a224088180c7dc641eadda1c2e"
+const URL = `https://gnews.io/api/v4/search?q=Google&lang=en&max=5&apikey=${APIKEY}`
+
+function goHome() {
+
+    document.getElementById('news').style.display = 'block'
+    document.getElementById('create-story').style.display = 'none'
+}
+
+function goForm() {
+
+    document.getElementById('news').style.display = 'none'
+    document.getElementById('create-story').style.display = 'block'
+}
+
+const data = fetch(URL).then((data) => data.json())
+
+console.log(data);
+
